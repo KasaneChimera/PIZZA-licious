33f0e7c336ab08deb1b972869a1fecfd94ab121e
KasaneChimera <sxayala15@email.com>
2026-05-29 02:29:35 -0400

    YOU'VE GIVEN ME ENOUGH PROBLEMS RECEIPTMANAGER

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,61 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Order {
+
+    private ArrayList<Pizza> pizzas = new ArrayList<>();
+    private ArrayList<Drink> drinks = new ArrayList<>();
+    private ArrayList<GarlicKnots> garlicKnots = new ArrayList<>();
+
+    public void addPizza(Pizza pizza) {
+        pizzas.add(pizza);
+    }
+
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+
+    public void addGarlicKnots(GarlicKnots knots) {
+        garlicKnots.add(knots);
+    }
+
+    public double calculateTotal() {
+
+        double total = 0;
+
+        for (Pizza pizza : pizzas) {
+            total += pizza.calculatePrice();
+        }
+
+        for (Drink drink : drinks) {
+            total += drink.getPrice();
+        }
+
+        for (GarlicKnots knots : garlicKnots) {
+            total += knots.getPrice();
+        }
+
+        return total;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Pizza pizza : pizzas) {
+            sb.append(pizza).append("\n");
+        }
+
+        for (Drink drink : drinks) {
+            sb.append(drink).append("\n");
+        }
+
+        for (GarlicKnots knots : garlicKnots) {
+            sb.append(knots).append("\n");
+        }
+
+        return sb.toString();
+    }
 }
--- src/main/java/com/pluralsight/ReceiptManager.java
+++ src/main/java/com/pluralsight/ReceiptManager.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptManager {
+
+    public static void saveReceipt(Order order) {
+
+        try {
+
+            DateTimeFormatter formatter =
+                    DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+
+            String fileName =
+                    "receipts/" +
+                            LocalDateTime.now().format(formatter)
+                            + ".txt";
+
+            PrintWriter pw =
+                    new PrintWriter(new FileWriter(fileName));
+
+            pw.println("===== PIZZA-licious Receipt =====");
+            pw.println();
+
+            pw.println(order);
+
+            pw.printf("TOTAL: $%.2f\n", order.calculateTotal());
+
+            pw.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Error saving receipt.");
+        }
+    }
 }
